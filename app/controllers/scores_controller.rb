f85db7b944ecf759f361f7562c8198ec30b77d3a
Justin Fortier <jfortier@pairshaped.ca>
2016-02-10 16:47:23 -0500

    Score routes

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,12 @@
 class ScoresController < ApplicationController
+
+  def index
+    @scores = Score.take(10)
+    render json: @scores
+  end
+
+  def status
+    @scores = Score.where("status = ?", params[:status].to_i)
+    render json: @scores
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+
+  get '/' => 'scores#index'
+  get '/status/:status' => 'scores#status'
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'scores#index'
+
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
